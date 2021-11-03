b0620e09c17828a19e06071e78780d778732d42e
natindo <lyskov256@gmail.com>
2021-11-03 16:13:11 +0300

    done

--- countingsort.c
+++ countingsort.c
@@ -1,6 +1,6 @@
 #include "utils.h"
 
-#define SIZE 1000000000
+#define SIZE 100
 
 int searchMax(long int a[]) {
     int max = a[0];
@@ -19,30 +19,26 @@ void methodOfCalculationCountingSort(long int lenght, long int arr[], long int h
 
     int NumberInArray = 0;
     for (size_t i = 0; i < lenght + 1; i++) {
-        int temp = i;
-        while(helpArr[temp]) {
-            arr[NumberInArray] = temp;
+        while(helpArr[i]) {
+            arr[NumberInArray] = i;
             NumberInArray++;
-            helpArr[temp]--;
+            helpArr[i]--;
         }
     }
 }
 
 int countingSort(void) {
-    //выделение памяти под массивы
+    //выделение памяти под массив
     long int *arr, *helpArr;
     
     arr = (long int *)malloc(SIZE * sizeof(long int));
     
     for (long int i = 0; i < SIZE; i++) { // Заполнение массива случайными числами
-        arr[i] = rand() % 10;
+        arr[i] = rand() % 201;
     }
     
-    int lenght = searchMax(arr);
-    helpArr = (long int *)malloc(lenght * sizeof(long int));
-    for (size_t i = 0; i < lenght; i++) {
-        helpArr[i] = 0;
-    }
+    int lenght = searchMax(arr); // поиск максимального значения в изначальном массиве
+    helpArr = (long int *)calloc(lenght, sizeof(long int));
 
     methodOfCalculationCountingSort(lenght, arr, helpArr); //сортировка методом подсчета
 
